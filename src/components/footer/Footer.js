551ecb0e3828a29dbd41f3b87747bc5a91e6d836
96955 <qwe123>
2020-06-04 18:52:29 +0800

    footer 界面

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,12 @@
 import React,{Component} from "react";
+import style from './footer.module.css'
 
 class Footer extends Component{
 
     render() {
         return (
-            <div>
-                Footer
+            <div className={style.footerWrapper}>
+                版权所有:慕课网&河畔一角(推荐使用谷歌浏览器,可以获得最佳操作体验) 技术支持:河畔一角
             </div>
         );
     }
